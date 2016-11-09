130973faeed85a343b4daa72a9bae7a966aca2bb
black-wild-boar <mihailkovalevskiy@gmail.com>
2016-11-09 22:30:48 +0700

    Lesson 1

--- part1-task1-1.rb
+++ part1-task1-1.rb
@@ -5,7 +5,7 @@ growth = gets.chomp
 ideal_weight = growth.to_i - 110
 
 if ideal_weight > 0
-	puts "Идеальный для вас вес #{ideal_weight} кг"
+  puts "Идеальный для вас вес #{ideal_weight} кг"
 else
-	puts "У вас идеальный вес!"
+  puts "У вас идеальный вес!"
 end
