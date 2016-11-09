b30e7c8a7aaec112f59ad8f0df53d63abff89a36
black-wild-boar <mihailkovalevskiy@gmail.com>
2016-11-09 23:34:56 +0700

    Lesson 1. Change variable name.

--- part1-task1-4.rb
+++ part1-task1-4.rb
@@ -14,14 +14,14 @@ discriminant = b**2 - 4*a*c
 if discriminant < 0
   puts "Дискриминант = #{discriminant}. Корней нет"
 elsif discriminant == 0
-  squera_root = Math.sqrt(discriminant)
+  square_root = Math.sqrt(discriminant)
   
   x12 = -b / (2 * a)
   puts "Дискриминант = #{discriminant}. Уравнение имеет 2 совпадающих вещественных корня #{x12}"
 else
-  squera_root = Math.sqrt(discriminant)
+  square_root = Math.sqrt(discriminant)
   
-  x1 = (-b + squera_root) / (2 * a)
-  x2 = (-b - squera_root) / (2 * a)
-  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} #{x2}"
+  x1 = (-b + square_root) / (2 * a)
+  x2 = (-b - square_root) / (2 * a)
+  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} и #{x2}"
 end
