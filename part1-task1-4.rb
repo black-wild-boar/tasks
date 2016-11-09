5c4188a9f78fd5cfd7cfafbbd708f526e7620b14
black-wild-boar <mihailkovalevskiy@gmail.com>
2016-11-09 22:35:00 +0700

    Lesson 1. Types to float & tabs to 2 spaces

--- part1-task1-4.rb
+++ part1-task1-4.rb
@@ -12,12 +12,12 @@ c = gets.chomp.to_f
 discriminant = b**2 - 4*a*c
 
 if discriminant > 0
-	x1 = Math.sqrt(a)
-	x2 = Math.sqrt(b)
-	puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} #{x2}"
+  x1 = Math.sqrt(a)
+  x2 = Math.sqrt(b)
+  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} #{x2}"
 elsif discriminant == 0
-	x1 = Math.sqrt(a)
-	puts "Дискриминант = #{discriminant}. Уравнение имеет 2 совпадающих вещественных корня #{x1}"
+  x1 = Math.sqrt(a)
+  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 совпадающих вещественных корня #{x1}"
 else
-	puts "Дискриминант = #{discriminant}. Корней нет"
-end	
+  puts "Дискриминант = #{discriminant}. Корней нет"
+end
