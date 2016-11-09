45fea937a8c520ca3fb6b8589d18f111d2fa6648
black-wild-boar <mihailkovalevskiy@gmail.com>
2016-11-09 23:25:19 +0700

    Lesson 1. Change formula.

--- part1-task1-4.rb
+++ part1-task1-4.rb
@@ -1,23 +1,27 @@
 puts "Добрый день!"
 
-puts "Введите первый коэффициент"
+puts "Введите первый коэффициент a"
 a = gets.chomp.to_f
 
-puts "Введите второй коэффициент"
+puts "Введите второй коэффициент b"
 b = gets.chomp.to_f
 
-puts "Введите третий коэффициент"
+puts "Введите третий коэффициент c"
 c = gets.chomp.to_f
 
 discriminant = b**2 - 4*a*c
 
-if discriminant > 0
-  x1 = Math.sqrt(a)
-  x2 = Math.sqrt(b)
-  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} #{x2}"
-elsif discriminant == 0
-  x1 = Math.sqrt(a)
-  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 совпадающих вещественных корня #{x1}"
-else
+if discriminant < 0
   puts "Дискриминант = #{discriminant}. Корней нет"
+elsif discriminant == 0
+  squera_root = Math.sqrt(discriminant)
+  
+  x12 = -b / (2 * a)
+  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 совпадающих вещественных корня #{x12}"
+else
+  squera_root = Math.sqrt(discriminant)
+  
+  x1 = (-b + squera_root) / (2 * a)
+  x2 = (-b - squera_root) / (2 * a)
+  puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} #{x2}"
 end
