9020fddfb9b6f6b11425f2a64f64fe6968a9ecba
mihailkovalevskiy <mihailkovalevskiy@gmail.com>
2016-11-09 17:58:50 +0700

    Lesson 1

---
+++ part1-task1-4.rb
@@ -0,0 +1,23 @@
+puts "Добрый день!"
+
+puts "Введите первый коэффициент"
+a = gets.chomp.to_i
+
+puts "Введите второй коэффициент"
+b = gets.chomp.to_i
+
+puts "Введите третий коэффициент"
+c = gets.chomp.to_i
+
+discriminant = b**2 - 4*a*c
+
+if discriminant > 0
+	x1 = Math.sqrt(a)
+	x2 = Math.sqrt(b)
+	puts "Дискриминант = #{discriminant}. Уравнение имеет 2 различных вещественных корня #{x1} #{x2}"
+elsif discriminant == 0
+	x1 = Math.sqrt(a)
+	puts "Дискриминант = #{discriminant}. Уравнение имеет 2 совпадающих вещественных корня #{x1}"
+else
+	puts "Дискриминант = #{discriminant}. Корней нет"
+end	
